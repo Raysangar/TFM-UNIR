4fefc7cfd7e7691b296ee29a2b47af5147039676
Rayco Sánchez García <raycosanchezgarcia@gmail.com>
2021-06-24 11:45:23 +0100

    Credtis and Settings screens implemented inside the Main Menu

--- Assets/Game/Scripts/UI/MainMenu/MainMenuController.cs
+++ Assets/Game/Scripts/UI/MainMenu/MainMenuController.cs
@@ -2,16 +2,31 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using Core.Utils.UI;
+using System.Collections;
 
 namespace Game.UI
 {
     [RequireComponent(typeof(GamepadMenuController))]
     public class MainMenuController : MonoBehaviour
     {
+        public enum State { Hub, Settings, Credits };
+
+        public State CurrentState { get; private set; }
+
+        [Header("HUB")]
+        [SerializeField] CanvasGroup hubParent;
         [SerializeField] Button continueButton;
         [SerializeField] Button newGameButton;
+        [SerializeField] Button creditsButton;
         [SerializeField] Button settingsButton;
         [SerializeField] Button exitButton;
+
+        [Header("Settings")]
+        [SerializeField] CanvasGroup settingsParent;
+        [SerializeField] Button settingsBackButton;
+
+        [Header("Credits")]
+        [SerializeField] CanvasGroup creditsParent;
 
         private GamepadMenuController gamepadMenuController;
 
@@ -23,6 +38,10 @@ namespace Game.UI
             newGameButton.onClick.AddListener(OnNewGameButtonClicked);
             settingsButton.onClick.AddListener(OnSettingsButtonClicked);
             exitButton.onClick.AddListener(OnExitButtonClicked);
+            creditsButton.onClick.AddListener(OnCreditsButtonClicked);
+            settingsBackButton.onClick.AddListener(OnSettingsBackButtonClicked);
+
+            SetState(State.Hub);
         }
 
         private void OnContinueButtonClicked()
@@ -37,12 +56,37 @@ namespace Game.UI
 
         private void OnSettingsButtonClicked()
         {
-
+            SetState(State.Settings);
         }
 
         private void OnExitButtonClicked()
         {
             Application.Quit();
+        }
+
+        private void OnCreditsButtonClicked()
+        {
+            SetState(State.Credits);
+            StartCoroutine(CreditsAnimation());
+        }
+
+        private IEnumerator CreditsAnimation()
+        {
+            yield return new WaitForSeconds(3);
+            SetState(State.Hub);
+        }
+
+        private void OnSettingsBackButtonClicked()
+        {
+            SetState(State.Hub);
+        }
+
+        private void SetState(State state)
+        {
+            CurrentState = state;
+            hubParent.gameObject.SetActive(state == State.Hub);
+            creditsParent.gameObject.SetActive(state == State.Credits);
+            settingsParent.gameObject.SetActive(state == State.Settings);
         }
     }
 }
