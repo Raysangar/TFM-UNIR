ee4625052d2146c4483cfc365713faa7f0bf5e02
Rayco Sánchez García <raycosanchezgarcia@gmail.com>
2021-09-06 17:04:07 +0100

    Setup pickups and enemies for outdoor level

--- Assets/Game/Scripts/Gameplay/PickUps/AmmoPickUp.cs
+++ Assets/Game/Scripts/Gameplay/PickUps/AmmoPickUp.cs
@@ -7,10 +7,17 @@ namespace Game.Gameplay.Pickups
     {
         [SerializeField] int ammoIndex;
         [SerializeField] int amount;
+        [SerializeField] GameObject[] renderersPerAmmoIndex;
 
         protected override void Activate(PlayerController player)
         {
             player.Weapon.AddAmmo(ammoIndex, amount);
+        }
+
+        protected override void OnEnable()
+        {
+            for (int i = 0; i < renderersPerAmmoIndex.Length; ++i)
+                renderersPerAmmoIndex[i].SetActive(i == ammoIndex);
         }
     }
 }
